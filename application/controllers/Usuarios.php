b54f595b65c541c34b1d4f4821e0a64eb426d58c
Alexandre do Vale <vale.apv@gmail.com>
2018-05-08 11:58:00 +0000

    Preparando o ambiente para prática do curso Criando Aplicações Web com o Framework PHP CodeIgniter 3

--- application/controllers/Usuarios.php
+++ application/controllers/Usuarios.php
@@ -8,7 +8,7 @@ class Usuarios extends CI_Controller {
             "email" => $this->input->post("email"),
             "senha" => md5($this->input->post("senha")) //encriptação MD5
         );
-        $this->load->database();   
+ 
         $this->load->model("MUsuarios");
         $this->MUsuarios->salva($usuario);
         $this->load->view("usuarios/novo");
